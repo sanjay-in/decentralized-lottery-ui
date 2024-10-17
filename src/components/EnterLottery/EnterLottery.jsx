d47c577cf0cacd37e7404ea40ced48c7853d08d7
sanjay-in <sanjaysridharan@outlook.com>
2024-10-17 23:45:09 +0530

    add screen loader

--- src/components/EnterLottery/EnterLottery.jsx
+++ src/components/EnterLottery/EnterLottery.jsx
@@ -13,8 +13,10 @@ const EnterLottery = () => {
   const [recentWinner, setRecentWinner] = useState("");
   const [copyClipboard, setCopyClipboard] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [screenLoading, setScreenLoading] = useState(false);
 
   const setLotteryContractHandler = async () => {
+    setScreenLoading(true);
     if (window.ethereum) {
       try {
         const provider = new ethers.BrowserProvider(window.ethereum);
@@ -30,6 +32,7 @@ const EnterLottery = () => {
     } else {
       toastMessage("error", "Unable to detect metamask");
     }
+    setScreenLoading(false);
   };
 
   const updateRecentWinner = async () => {
@@ -112,44 +115,50 @@ const EnterLottery = () => {
   }, []);
   return (
     <div className="lottery">
-      <p className="lottery-text">Enter the amount you need to give for lottery</p>
-      <div className="lottery-amount">
-        <div className="lottery-amount-symbol" aria-hidden="true">
-          ETH
-        </div>
-        <input
-          className="lottery-amount-value"
-          type="number"
-          placeholder="0.001"
-          step={0.001}
-          min={0.001}
-          value={ethAmount}
-          onChange={(e) => setEthAmount(e.target.value)}
-        />
-      </div>
-      <div className="lottery-amount-info">
-        <i className="fa fa-info-circle" aria-hidden="true"></i>
-        <div className="lottery-amount-info-text">Minimum amount to enter raffle is 0.001 ETH</div>
-      </div>
-      <div>
-        <div className="lottery-recent-winner">
-          {recentWinner ? (
-            <div className="lottery-recent-winner-container">
-              Recent Winner: {trimmedAddress(recentWinner)}{" "}
-              <div className="lottery-tooltip">
-                <i className="fa fa-clone lottery-clone-icon" aria-hidden="true" onClick={copyToClipboard}></i>
-                <span className="lottery-tooltip-text">{copyClipboard ? "Copied!" : "Copy address"}</span>
-              </div>
+      {screenLoading ? (
+        <Spinner variant="warning" className="lottery-loading-page" animation="border" />
+      ) : (
+        <>
+          <p className="lottery-text">Enter the amount you need to give for lottery</p>
+          <div className="lottery-amount">
+            <div className="lottery-amount-symbol" aria-hidden="true">
+              ETH
             </div>
-          ) : (
-            "No recent winner"
-          )}
-        </div>
-      </div>
-      <Button id="lottery-btn" disabled={loading} onClick={enterLottery}>
-        <span className="lottery-btn-txt">{loading ? <Spinner className="lottery-loading-spinner" animation="border" /> : "Enter"}</span>
-      </Button>
-      <ToastContainer />
+            <input
+              className="lottery-amount-value"
+              type="number"
+              placeholder="0.001"
+              step={0.001}
+              min={0.001}
+              value={ethAmount}
+              onChange={(e) => setEthAmount(e.target.value)}
+            />
+          </div>
+          <div className="lottery-amount-info">
+            <i className="fa fa-info-circle" aria-hidden="true"></i>
+            <div className="lottery-amount-info-text">Minimum amount to enter raffle is 0.001 ETH</div>
+          </div>
+          <div>
+            <div className="lottery-recent-winner">
+              {recentWinner ? (
+                <div className="lottery-recent-winner-container">
+                  Recent Winner: {trimmedAddress(recentWinner)}{" "}
+                  <div className="lottery-tooltip">
+                    <i className="fa fa-clone lottery-clone-icon" aria-hidden="true" onClick={copyToClipboard}></i>
+                    <span className="lottery-tooltip-text">{copyClipboard ? "Copied!" : "Copy address"}</span>
+                  </div>
+                </div>
+              ) : (
+                "No recent winner"
+              )}
+            </div>
+          </div>
+          <Button id="lottery-btn" disabled={loading} onClick={enterLottery}>
+            <span className="lottery-btn-txt">{loading ? <Spinner className="lottery-loading-spinner" animation="border" /> : "Enter"}</span>
+          </Button>
+          <ToastContainer />
+        </>
+      )}
     </div>
   );
 };
